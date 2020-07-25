5e5133cec7f45e31260215dc2160061a47eeb390
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-07-25 16:48:13 +0100

    server

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
+server.use(express.static("public"))
 
+server.set("view engine", "njk")
 
-server.get("/", function(req, res) {
-    return res.send("hello world!")
+nunjucks.configure("views", {
+    express: server
 })
 
+// Rota
+server.get("/", function(req, res) {
+    return res.render("about")
+})
+
+// Rota
+server.get("/portfolio", function(req, res) {
+    return res.render("portfolio")
+})
+
+// starting server
 server.listen(5000, function() {
     console.log("server is running!!")
 })
