509af7a8db926316ba13bd5bbf770a1970dc22a5
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-07-27 09:31:05 +0100

    up

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "BXqUH86F-kA",
+        title: "Curso de JavaScript",
+        duration: "33 aulas",
+        price: "FREE"
+    },
+    {
+        id: "F7KzJ7e6EAc",
+        title: "Curso PHP Iniciante",
+        duration: "24 aulas",
+        price: "FREE"
+    },
+    {
+        id: "zpK_MqEMgu4",
+        title: "Curso de Hadware",
+        duration: "34 aulas",
+        price: "FREE"
+    },
+    {
+        id: "sTX0UEplF54",
+        title: "Curso de Java",
+        duration: "32 aulas",
+        price: "FREE"
+    },
+    {
+        id: "Ofktsne-utM",
+        title: "Curso de Mysql",
+        duration: "19 aulas",
+        price: "FREE"
+    },
+    {
+        id: "xEKo29OWILE",
+        title: "Curso de Git e GitHub",
+        duration: "13 aulas",
+        price: "FREE"
+    }
+]
--- server.js
+++ server.js
@@ -2,23 +2,37 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 server.use(express.static("public"))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 // Rota
 server.get("/", function(req, res) {
-    return res.render("about")
+    const about = {
+        avatar_url: 'https://avatars2.githubusercontent.com/u/65118827?s=460&u=7c508a738d926eb0f20fdef0502bd2aab874f51c&v=4',
+        name: 'Wanderson Oliveira',
+        role: 'Web Developer',
+        description: 'Estudante da <a href="https://origamid.com" target="_black">Origamid</a> e da <a href="https://rockseat.com.br" target="_black">Rockseat</a>. Futuro Desenvolvedor Full-Steack.',
+        links: [
+            {name: 'GitHub', url: 'https://github.com/wanderson1873'},
+            {name: 'Twitter', url: 'https://twitter.com/wanderson1873'},
+            {name: 'Linkedin', url: 'https://www.linkedin.com/in/wanderson1873/'}
+        ]
+    }
+
+    return res.render("about", { about })
 })
 
 // Rota
 server.get("/portfolio", function(req, res) {
-    return res.render("portfolio")
+    return res.render("portfolio", { itens: videos })
 })
 
 // starting server
