030b8ee7bc2ba917cc9e65b996284457b8020a60
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-08-21 18:14:38 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wanderson Oliveira</title>
+</head>
+<body>
+    <header>
+        <li><a href="">Sobre</a></li>
+        <li><a href="">Formação</a></li>
+        <li><a href="">Projetos</a></li>
+    </header>
+    
+    <main>
+    <img src="" alt="">
+        <h1>Wanderon <span>Oliveira</span></h1>
+        <h2>Junior Web Developer</h2>
+    </main>
+</body>
+</html>
